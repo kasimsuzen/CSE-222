645faebd2644ebfa3df6691abac7f2fa91f6eb55
kasimsuzen <kasmszen@gmail.com>
2016-02-25 18:12:54 +0200

    Teacher test implemented

---
+++ HW01/src/test/java/TeacherTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kasım Süzen
+ * Created by siyah on 24.02.2016.
+ */
+public class TeacherTest {
+
+    @Test
+    public void testAddDocumentToClass() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAssignment() throws Exception {
+
+    }
+}
