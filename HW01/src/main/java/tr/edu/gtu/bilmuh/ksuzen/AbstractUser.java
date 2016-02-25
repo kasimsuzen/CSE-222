c13b984fd7aff07e86569093aed1646897420589
kasimsuzen <kasmszen@gmail.com>
2016-02-25 18:01:37 +0200

    Lack of documentation of abstractUser handled

--- HW01/src/main/java/tr/edu/gtu/bilmuh/ksuzen/AbstractUser.java
+++ HW01/src/main/java/tr/edu/gtu/bilmuh/ksuzen/AbstractUser.java
@@ -1,30 +1,56 @@
 package tr.edu.gtu.bilmuh.ksuzen;
 
 /**
+ * Every user of this program must extends this Abstract Class
  * @author Kasım Süzen
- * Created by siyah on 20.02.2016.
+ * Created on 20.02.2016
  */
 public class AbstractUser implements userInterface{
     private int IDNo;
     private String name;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getIDNo() {
         return IDNo;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setIDNo(int IDNo) {
         this.IDNo = IDNo;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * Returns name and id number of called user
+     * @return name and id number of called user
+     */
+    @Override
+    public String toString() {
+        String temp = "";
+
+        temp += getName();
+        temp += Integer.toString(getIDNo());
+        return temp;
     }
 }
