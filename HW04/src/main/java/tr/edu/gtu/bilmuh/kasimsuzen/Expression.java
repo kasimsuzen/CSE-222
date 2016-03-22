9add919a631e1877a6bd92a711cbcebea0089f3f
kasimsuzen <kasmszen@gmail.com>
2016-03-23 00:03:02 +0200

    Expression class implemented

---
+++ HW04/src/main/java/tr/edu/gtu/bilmuh/kasimsuzen/Expression.java
@@ -0,0 +1,36 @@
+package tr.edu.gtu.bilmuh.kasimsuzen;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kasım Süzen on 22.03.2016.
+ *
+ */
+public class Expression {
+    private ArrayList<String> lines;
+
+    public Expression(){
+        lines = new ArrayList<String>();
+    }
+
+    public void addExp(String expData){
+        lines.add(expData);
+    }
+
+    public String getExp(int index){
+        return lines.get(index);
+    }
+
+    public int getSize(){
+        return lines.size();
+    }
+
+    public void clear(){
+        lines.clear();
+    }
+
+    @Override
+    public String toString(){
+        return lines.toString();
+    }
+}
